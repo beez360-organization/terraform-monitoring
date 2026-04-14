2be0063d2dc45e2fe8d560a320f5b15a7c1a1466
oumaima-benhadid <oumaima.benhadid@binov.com>
2026-04-14 11:02:13 +0100

    fix main.tf de vm_log_traces

--- modules/vm_logs_traces/main.tf
+++ modules/vm_logs_traces/main.tf
@@ -1,3 +1,6 @@
+##############################
+# NSG
+##############################
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.vm_name}-nsg"
   location            = var.location
@@ -160,17 +163,53 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
+##############################
+# Public IP
+##############################
+resource "azurerm_public_ip" "this" {
+  name                = "${var.vm_name}-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+##############################
+# NIC
+##############################
+resource "azurerm_network_interface" "this" {
+  name                = "${var.vm_name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.this.id
+  }
+
+  tags = var.tags
+}
+
+##############################
+# VM
+##############################
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = var.vm_name
   location            = var.location
   resource_group_name = var.resource_group_name
   size                = "Standard_B2ms"
 
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
-  network_interface_ids = [azurerm_network_interface.this.id]
+  network_interface_ids = [
+    azurerm_network_interface.this.id
+  ]
 
   identity {
     type = "SystemAssigned"
--- modules/vm_logs_traces/outputs.tf
+++ modules/vm_logs_traces/outputs.tf
@@ -6,7 +6,7 @@ output "vm_id" {
 
 output "private_ip" {
   description = "IP privée de la VM Logs/Traces"
-  value       = azurerm_network_interface.nic.private_ip_address
+  value       = azurerm_network_interface.this.private_ip_address
 }
 output "public_ip_address" {
   value = azurerm_public_ip.this.ip_address
