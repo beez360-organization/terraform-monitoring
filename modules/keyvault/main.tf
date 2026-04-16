97abe9f9fe26609d60e8998b2eb7c528363890fc
oumaima-benhadid <oumaima.benhadid@binov.com>
2026-04-16 21:27:29 +0100

    feat : update access policies

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -6,30 +6,19 @@ resource "azurerm_key_vault" "this" {
 
   sku_name = "standard"
 
-  enable_rbac_authorization = false
+  enable_rbac_authorization = true
 
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
   tags = var.tags
 }
-
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault_access_policy" "current_user" {
-  key_vault_id = azurerm_key_vault.this.id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete"
-  ]
-    depends_on = [
-    azurerm_key_vault.this
-  ]
-
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
+
+
